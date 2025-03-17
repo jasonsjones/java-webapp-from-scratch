e3938804e9f8a128c1b8427fbab531861bf9d7de
Jason Jones <jsjones96@gmail.com>
2025-03-16 17:41:24 -0700

    refactor: add connection handler thread

---
+++ core-app/src/main/java/com/jasonsjones/app/core/ConnectionHandler.java
@@ -0,0 +1,80 @@
+package com.jasonsjones.app.core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class ConnectionHandler extends Thread {
+
+    private static final String CRLF = "/r/n";
+    private Socket socket;
+
+    public ConnectionHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("\nNew connection from: " + this.socket.getInetAddress() + "\n");
+        try {
+            handleRequest(socket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.socket != null) {
+                try {
+                    this.socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Connection closed");
+        }
+    }
+
+    private static void handleRequest(Socket clientSocket) throws IOException {
+        echoRequest(clientSocket);
+
+        String httpResponse = generateResponseHeader("text/html", "UTF-8") + generateResponseContent();
+ 
+        OutputStream outputStream = clientSocket.getOutputStream();
+        outputStream.write(httpResponse.getBytes());
+        outputStream.flush();
+    }
+
+    private static void echoRequest(Socket clientSocket) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String line;
+        StringBuilder requestData = new StringBuilder();
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            requestData.append(line).append("\r\n");
+        }
+        System.out.println("Received request:\n" + requestData);
+    }
+
+    private static String generateResponseHeader(String mimeType, String charset) {
+        String header = "HTTP/1.1 200 OK" + CRLF 
+                + "Content-Type: " + mimeType + "; charset=" + charset + CRLF
+                + "Connection: close" + CRLF
+                + CRLF;
+
+        return header;
+    }
+
+    private static String generateResponseContent() {
+        String httpResponseContent = "<!DOCTYPE html>"
+                + "<html lang=\"en\">"
+                + "<head><title>Simple HTTP Server</title></head>"
+                + "<body><h1>Hello, World!</h1></body>"
+                + "</html>"
+                + CRLF;
+
+        return httpResponseContent;
+    }
+
+    
+}
--- core-app/src/main/java/com/jasonsjones/app/core/SocketListener.java
+++ core-app/src/main/java/com/jasonsjones/app/core/SocketListener.java
@@ -1,9 +1,6 @@
 package com.jasonsjones.app.core;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,7 +8,6 @@ import com.jasonsjones.app.config.Configuration;
 
 public class SocketListener extends Thread {
 
-    private static final String CRLF = "/r/n";
     private int port;
         
     public SocketListener(Configuration config) {
@@ -20,64 +16,18 @@ public class SocketListener extends Thread {
 
     @Override
     public void run() {
-        System.out.println("ServerListenerThread running");
-        Socket clientSocket = null;
+        System.out.println("Socket listenter running");
         try (ServerSocket serverSocket = new ServerSocket(this.port)) {
+
             System.out.println("\nWeb server is running on port " + this.port);
 
-            clientSocket = serverSocket.accept();
-            System.out.println("\nNew connection from: " + clientSocket.getInetAddress() + "\n");
-            handleRequest(clientSocket);
-        } catch (IOException e) {
-            System.err.println("Could not start server" + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            while (serverSocket.isBound() && !serverSocket.isClosed()) {
+                Socket clientSocket = serverSocket.accept();
+                ConnectionHandler connectionHandler = new ConnectionHandler(clientSocket);
+                connectionHandler.start();
             }
+        } catch (IOException e) {
+            System.err.println("Problem occurred setting up socket" + e.getMessage());
         }
-    }
-
-    private static void handleRequest(Socket clientSocket) throws IOException {
-        echoRequest(clientSocket);
-
-        String httpResponse = generateResponseHeader("text/html", "UTF-8") + generateResponseContent();
- 
-        OutputStream outputStream = clientSocket.getOutputStream();
-        outputStream.write(httpResponse.getBytes());
-        outputStream.flush();
-    }
-
-    private static void echoRequest(Socket clientSocket) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String line;
-        StringBuilder requestData = new StringBuilder();
-        while ((line = reader.readLine()) != null && !line.isEmpty()) {
-            requestData.append(line).append("\r\n");
-        }
-        System.out.println("Received request:\n" + requestData);
-    }
-
-    private static String generateResponseHeader(String mimeType, String charset) {
-        String header = "HTTP/1.1 200 OK" + CRLF 
-                + "Content-Type: " + mimeType + "; charset=" + charset + CRLF
-                + "Connection: close" + CRLF
-                + CRLF;
-
-        return header;
-    }
-
-    private static String generateResponseContent() {
-        String httpResponseContent = "<!DOCTYPE html>"
-                + "<html lang=\"en\">"
-                + "<head><title>Simple HTTP Server</title></head>"
-                + "<body><h1>Hello, World!</h1></body>"
-                + "</html>"
-                + CRLF;
-
-        return httpResponseContent;
     }
 }
